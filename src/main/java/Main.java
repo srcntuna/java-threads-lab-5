e18bdbb53f7c0b8a29e1f8cee68d88ae7792e2d5
Sercan Tuna <ssercantuna@hotmail.com>
2022-07-26 13:14:35 -0400

    done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // your code here
+        SavingsAccount savingsAccount = new SavingsAccount();
+
+       Thread t1 = new Thread(()->savingsAccount.deposit(10_000));
+       Thread t2 = new Thread(()->savingsAccount.withdraw(5_000));
+       Thread t3 = new Thread(()->savingsAccount.withdraw(15_000));
+       Thread t4 = new Thread(()->savingsAccount.withdraw(5_000));
+
+       t1.setName("thread-1");
+       t2.setName("thread-2");
+       t3.setName("thread-3");
+       t4.setName("thread-4");
+
+
+       t1.start();
+       t2.start();
+       t3.start();
+       t4.start();
+
+       t1.join();
+       t2.join();
+       t3.join();
+       t4.join();
+
+        System.out.println(savingsAccount.getTotal());
+
     }
 }
--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -1,16 +1,35 @@
-public class SavingsAccount {
+public class SavingsAccount  {
 
   private long total = 0;
 
   public boolean withdraw(long amount) {
-      
+      synchronized (this){
+          if(total<amount){
+              System.out.println("Error! insufficient funds! . rejected process for "+Thread.currentThread().getName());
+              return false;
+          }
+
+          total-=amount;
+
+          return true;
+
+
+      }
+
+
   }
 
-  public void deposit(long amount) {
+  public  void deposit(long amount) {
+
+      synchronized (this){
+          total+=amount;
+      }
+
 
   }
 
   public long getTotal() {
       return total;
   }
+
 }
